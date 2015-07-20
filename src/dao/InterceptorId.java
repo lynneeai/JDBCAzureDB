681cba6fb5882dbaf32c5bbf46b8398de46f442e
lynneeai <lynneeai@hotmail.com>
2015-07-20 16:14:58 -0400

    interceptorId

--- src/dao/InterceptorId.java
+++ src/dao/InterceptorId.java
@@ -1,9 +1,32 @@
 package src.dao;
 
-public class InterceptorId {
-
-	public InterceptorId() {
-		// TODO Auto-generated constructor stub
+public class InterceptorId 
+{
+	public String intSerial;
+	public String embeddedId;
+	public String key;
+	
+	public InterceptorId() {}
+	
+	public InterceptorId(String intserial, String embeddedid, String Key)
+	{
+		intSerial = intserial;
+		embeddedId = embeddedid;
+		key = Key;
 	}
-
+	
+	public String getIntSerial()
+	{
+		return intSerial;
+	}
+	
+	public String getEmbeddedId()
+	{
+		return embeddedId;
+	}
+	
+	public String getKey()
+	{
+		return key;
+	}
 }
