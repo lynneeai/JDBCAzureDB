4d4faa31003cf53b8dbca4020ec8fe2c08faa7df
lynneeai <lynneeai@hotmail.com>
2015-07-20 11:59:27 -0400

    user classes

---
+++ src/UserDao.java
@@ -0,0 +1,25 @@
+package src;
+
+public class UserDao
+{
+	private User _user;
+	public User getUser()
+	{
+		
+		
+		
+		
+		return _user;
+	}
+	
+	public void createUser()
+	{
+		
+	}
+	
+	public void createAdminUser()
+	{
+		
+	}
+	
+}
