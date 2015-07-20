217bfcd2bac6fa2c1378f0a35cfd226f43f4aabf
kleril <kleril@users.noreply.github.com>
2015-07-20 15:44:29 -0400

    IntIdDao

--- src/dao/InterceptorIdDao.java
+++ src/dao/InterceptorIdDao.java
@@ -1,9 +1,57 @@
 package src.dao;
 
-public class InterceptorIdDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-	public InterceptorIdDao() {
-		// TODO Auto-generated constructor stub
+import src.ConnectToSQLAzure;
+
+public class InterceptorIdDao {
+	private InterceptorId id;
+	private String dbName = ConnectToSQLAzure.getActiveDB();
+	private String connString = "jdbc:sqlserver://zypnl8g76k.database.windows.net:1433;"
+			+ "database="+ dbName +";"
+			+ "user=CozDev01_DBA!Us3rAcc0unt@zypnl8g76k;"
+			+ "password=Ecru9278Fudge;"
+			+ "encrypt=true;"
+			+ "hostNameInCertificate=*.database.windows.net;"
+			+ "loginTimeout=30;";
+	private Connection conn = null;
+	private Statement stmt = null;  
+	private String sqlString;
+	
+	public InterceptorIdDao(InterceptorId id) {
+		this.id = id;
 	}
 
+	
+	public InterceptorId getInterceptorId()
+	{
+		return id;
+	}
+
+	public void createInterceptorId()
+	{
+		try
+		{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			conn = DriverManager.getConnection(connString);
+
+			sqlString = "INSERT INTO tblInterceptorId "
+					+ "VALUES ("+ id.getIntSerial() + ", "
+					+ id.getEmbeddedId() + ", " 
+					+ id.getKey() + ")";
+
+			stmt.executeUpdate(sqlString);
+
+			conn.close();
+
+		}
+		catch (Exception e)
+		{
+
+		}
+	}
+	
 }
